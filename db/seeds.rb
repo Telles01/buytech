75cc05cd9af5c140feeddcf0000e0358f449d360
Alexandre Ribeiro <telonas@gmail.com>
2023-08-29 12:03:05 -0300

    seeds ongoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+peter = User.create!({
+  name: "Peter",
+  surname: "Parker",
+  address: "Queens",
+  city: "New York",
+  phone_number: 987987432,
+  email: "spidey@gmail.com",
+  password: "123123"
+  })
+  # product = Product.new
+  # product.owner = peter
+  # product.save
+
+User.create!({
+  name: "Steve",
+  surname: "Rogers",
+  address: "Brooklyn",
+  city: "New York",
+  phone_number: 987987412,
+  email: "cap@gmail.com",
+  password: "123123"
+  })
+User.create!({
+  name: "Tony",
+  surname: "Stark",
+  address: "Malibu",
+  city: "Los Angeles",
+  phone_number: 987966432,
+  email: "iamironman@gmail.com",
+  password: "123123"
+})
+
+p "Created #{User.count} users"
