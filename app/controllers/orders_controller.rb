4fa77b141e52b134b3b5217ed324eaafd4d6835f
victor silva <canalampmoficial2018@gmail.com>
2023-08-30 13:37:05 -0300

    order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,12 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def order_confirmation
+    # @user = User.find(params[:id])
+    @user = current_user
+    redirect_to order_confirmation_path(@user)
+  end
+
   def create
     @order = Order.new(order_params)
     @order.buyer = current_user # Assuming you have a current_user method available
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "products#index"
 
   resources :products do
     get "user", on: :collection
     resources :orders, only: [:new, :create]
   end
 
- 
+  get '/orderConfirmation', to: 'orders#order_confirmation'
 end
